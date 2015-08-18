bda36766180d8130c79edba0b4ec366eef4c1f0d
NishaJeffrey <nishakpaul@gmail.com>
2015-08-18 10:59:00 -0400

    SalestaxAssignment

--- SalesTaxCalculator/WebContent/sales_tax.js
+++ SalesTaxCalculator/WebContent/sales_tax.js
@@ -2,3 +2,26 @@ var $ = function (id) {
     return document.getElementById(id); 
 }
 
+var calculate_click=function(){
+	var subtotal=parseFloat($("subtotal").value);
+	var taxRate=parseFloat($("taxRate").value);
+	$("salesTax").value="";
+	$("total").value="";
+	
+	if(isNaN(subtotal) || subtotal<0){
+		alert("Subtotal must be a positive number!");
+	}else if(isNaN(taxRate) || taxRate<0){
+		alert("Taxrate must be a positive number!");
+	}else{
+		var salesTax=subtotal*(taxRate/100);
+		salesTax=parseFloat(salesTax.toFixed(2));
+		var total=subtotal+salesTax;
+		$("salesTax").value=salesTax;
+		$("total").value=total.toFixed(2);
+	}
+}
+	
+window.onload=function(){
+	$("calculate").onclick=calculate_click;
+	$("subtotal").focus;
+}
